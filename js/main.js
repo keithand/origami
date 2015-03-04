9af9f2dc359fa6948bfbfa37f6232a226f8df9f7
Keith Anderson <keith.quiche@gmail.com>
2015-03-04 11:43:35 -0800

    Version 0.0.3
    Starting on the javascript for adding new categories.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
 // JQuery Setup
 $(document).ready(function(){
+
+	var closeButton = function(){
+		$("#overlay-close-button").on("click", function(){
+			$("#overlay-back, #overlay-html-add-category").fadeOut(200);		
+		});
+	};
+
 	$("#add-category").on("click", function(){
-		event.presentDefault();
+		event.preventDefault();
 		event.stopPropagation();
+		$("#overlay-back, #overlay-html-add-category").fadeIn(200);
+		if($("#overlay-add-category-text").length >=1){
+			
+			}
 	});
+	
+	
+	
+	closeButton();
 });
